9912f9b90e2ce4a67773d30bc6edf8f2d804bb35
Luiz Fellipe <110192027+luizfellips@users.noreply.github.com>
2022-08-26 14:57:50 -0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-frame = pd.read_excel('_files\\tabela mgo.xlsx')
-frame['PREÇO'] = frame['PREÇO'].apply(lambda x: x.replace(',','.').strip() if isinstance(x,str) else x)
-frame = frame.astype({"PREÇO": float})
-frame = frame.sort_values(by=['PREÇO'])
-print(frame)
